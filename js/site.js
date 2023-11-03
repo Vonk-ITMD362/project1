478a37cb330b925e5fcd7c3f8b1553524748c045
cvonk02 <cvonk@hawk.iit.edu>
2023-11-03 00:32:14 -0500

    Updated js
    Added js function to alert when submit button is clicked.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,15 @@
 // jQuery 3.x-style ready event and locally scoped $
+
+$(document).ready(function () {
+  $("#news-form").click(function () {
+    alert("Thank you for subscribing to the Car Newsletter, you will recieve an email with more information soon.");
+  });
+}); 
+
+
 jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
-  });
+  }
+);
+
